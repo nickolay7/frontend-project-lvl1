3c89269343c56f0f27a4e19d23632eca7c8af4c4
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-08 18:29:03 +0300

    correct import and change " to '

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import {userName} from '../index.js';
+import userName from '../index';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 const user = userName();
 console.log(`Hello, ${user}!`);
