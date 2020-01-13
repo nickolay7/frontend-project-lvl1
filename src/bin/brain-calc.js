add2db585f886630fdf160cbf20b0d09d4c62291
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-13 21:12:14 +0300

    add const generate, operate, add import engine in brain-calc.js. linter correct in brain-even.js. linter correct in index.js. start to brain-calc in Makefile.

--- src/bin/brain-calc.js
+++ src/bin/brain-calc.js
@@ -1,7 +1,33 @@
 #!/usr/bin/env node
 
+import { cons } from '@hexlet/pairs';
 import begin from './brain-games';
-import { even } from '../index';
+import { engine, rand } from '../index';
 
+const operate = () => {
+  const set = ['+', '-', '*'];
+  return set[Math.floor(Math.random() * 3)];
+};
+const generate = () => {
+  const operand1 = rand();
+  const operand2 = rand();
+  const operator = operate();
+  const quest = `${operand1} ${operator} ${operand2}`;
+  let answer;
+  switch (operator) {
+    case '*':
+      answer = operand1 * operand2;
+      break;
+    case '+':
+      answer = +operand1 + +operand2;
+      break;
+    case '-':
+      answer = operand1 - operand2;
+      break;
+    default:
+      break;
+  }
+  return cons(quest, String(answer));
+};
 const userName = begin('What is the result of the expression?');
-//calc(userName);
+engine(userName, generate);
