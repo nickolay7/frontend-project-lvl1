0b357728b7a690601bea3d507e3a0c2063648da2
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-13 21:16:30 +0300

    linter correct in brain-even.js.

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
+import { cons } from '@hexlet/pairs';
 import begin from './brain-games';
 import { engine, rand } from '../index';
-import { cons } from '@hexlet/pairs';
 
 const generate = () => {
-  let quest = rand();
-  let answer = quest % 2 === 0 ? 'yes' : 'no';
+  const quest = rand();
+  const answer = quest % 2 === 0 ? 'yes' : 'no';
   return cons(quest, answer);
 };
 const userName = begin('Answer "yes" if the number is even, otherwise answer "no".');
