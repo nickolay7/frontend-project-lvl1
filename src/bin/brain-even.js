b2530eafe9dca5f2daf70fdb82f8ccf715150859
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-13 19:29:47 +0300

    add import engine rand cons, add const generate in brain-even and correct import in index.js

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 
 import begin from './brain-games';
-import { even } from '../index';
+import { engine, rand } from '../index';
+import { cons } from '@hexlet/pairs';
 
+const generate = () => {
+  let quest = rand();
+  let answer = quest % 2 === 0 ? 'yes' : 'no';
+  return cons(quest, answer);
+};
 const userName = begin('Answer "yes" if the number is even, otherwise answer "no".');
-even(userName);
+engine(userName, generate);
