12a9b9334cebd1ab40a8adefab4665ede43690d5
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-16 21:08:06 +0300

    add shebang and x mode to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+#!/usr/bin/env node
+
 import readlineSync from 'readline-sync';
 import { car, cdr } from '@hexlet/pairs';
 
 const actual = (query) => readlineSync.question(query);
 export const rand = () => Math.floor(Math.random() ** 2 * 100);
-export const engine = (user, func) => {
+export const engine = (user, func) => {quest
   let pair = func();
   for (let i = 1; i !== 4; i += 1) {
     const quest = car(pair);
