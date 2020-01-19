b7a45214041a855ecb1e39508d8856d3c49612f9
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-19 14:02:41 +0300

    add file brain-gcd

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import { cons } from '@hexlet/pairs';
+import begin from './brain-games';
+import { engine, rand } from '../index';
+
+const generate = () => {
+  let num1 = rand();
+  let num2 = rand();
+  const quest = `${num1} ${num2}`;
+  while (num1 !== num2) {
+    (num1 > num2) ? (num1 -= num2) : (num2 -= num1);
+  }
+  return cons(quest, String(num1));
+};
+export default () => {
+  const userName = begin('Find the greatest common divisor of given numbers.');
+  engine(userName, generate);
+};
