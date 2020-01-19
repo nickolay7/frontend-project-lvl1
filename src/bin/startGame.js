8481dfb88d094029652a203863f2703c5e2a5a0f
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-19 21:33:00 +0300

    add import brainProgression and case

--- src/bin/startGame.js
+++ src/bin/startGame.js
@@ -3,10 +3,11 @@
 import brainEven from './brain-even';
 import brainCalc from './brain-calc';
 import brainGcd from './brain-gcd';
+import brainProgression from './brain-progression';
 
 import actual from '../index';
 
-console.log('Please select game : "brain-even: 1, brain-calc: 2, brain-gcd: 3"');
+console.log('Please select game : "brain-even: 1, brain-calc: 2, brain-gcd: 3, brain-progression: 4"');
 const gameName = actual('Select;)');
 switch (gameName) {
   case '1':
@@ -17,6 +18,9 @@ switch (gameName) {
     break;
   case '3':
     brainGcd();
+    break;
+  case '4':
+    brainProgression();
     break;
   default:
     break;
