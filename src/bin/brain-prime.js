d9487da5910e7c5d8aaafa627ec3e657f39ef2d2
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-20 19:44:46 +0300

    modify brain-prime.js

---
+++ src/bin/brain-prime.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { cons } from '@hexlet/pairs';
+import begin from './brain-games';
+import { engine, rand } from '../index';
+
+const isPrime = (x) => {
+  if (x < 2) return false;
+
+  for (let i = x - 1; i > 1; i -= 1) {
+    if (x % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const generate = () => {
+  const quest = rand(1, 101);
+  const answer = isPrime(quest) ? 'yes' : 'no';
+  return cons(quest, answer);
+};
+
+export default () => {
+  const userName = begin('Answer "yes" if given number is prime. Otherwise answer "no".');
+  engine(userName, generate);
+};
