44476243aa5618208cba04dae3985e7dc3752620
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-24 12:11:08 +0300

    refactor const getNumber

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,8 +1,8 @@
 import { cons } from '@hexlet/pairs';
-import { engine, getNumber } from '../index';
+import { engine, getNumber, max, min } from '../index';
 
 const generate = () => {
-  const quest = getNumber(1, 101);
+  const quest = getNumber(min, max);
   const answer = quest % 2 === 0 ? 'yes' : 'no';
   return cons(quest, answer);
 };
