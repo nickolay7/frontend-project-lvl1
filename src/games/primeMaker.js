20294a8094f62d7cf9ea1e35835df94d310a024c
Nickolai Semutkin <shkelal50@gmail.com>
2020-01-28 17:56:09 +0300

    refactor const isPrime

--- src/games/primeMaker.js
+++ src/games/primeMaker.js
@@ -6,7 +6,7 @@ import {
 const isPrime = (x) => {
   if (x < 2) return false;
 
-  for (let i = x - 1; i > 1; i -= 1) {
+  for (let i = 2; i <= x / 2; i += 1) {
     if (x % i === 0) {
       return false;
     }
